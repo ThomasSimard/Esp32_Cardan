6b648effcfa091eaefd99e06c910d2c936b53630
Thomas Simard <thsim31@ulaval.ca>
2024-04-13 01:20:14 -0400

    Working two client with 7k5SPS each

--- Server/Server.ino
+++ Server/Server.ino
@@ -13,15 +13,36 @@
  
 #include "DataStructure.h"
 
-unsigned count;
+constexpr uint8_t esp_mac1[] = {0x84, 0xCC, 0xA8, 0x61, 0x01, 0x4C};
+
+unsigned count1, count2;
+
+bool CompareMacAddress(const uint8_t * mac0, const uint8_t * mac1) {
+  return (mac0[0] == mac1[0] &&
+        mac0[1] == mac1[1] &&
+        mac0[2] == mac1[2] &&
+        mac0[3] == mac1[3] &&
+        mac0[4] == mac1[4] &&
+        mac0[5] == mac1[5]);
+}
 
 // Callback function executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&packet_buffer, incomingData, PACKET_SIZE * sizeof(char));
+  memcpy(&packet_buffer, incomingData, PACKET_SIZE * sizeof(char)); //Maybe we can take the incoming data directly OPTIMISATION!
   
-  count++;
+  Serial.print("\t");
+
+  if(CompareMacAddress(mac, esp_mac1)){
+    count1++;
+    Serial.print(count1);
+    Serial.print("\t1");
+  }
+  else{
+    Serial.print(count2);
+    Serial.print("\t2");
+    count2++;
+  }
   
-  Serial.print(count);
   Serial.print("\tData received: ");
   Serial.print(len);
 
