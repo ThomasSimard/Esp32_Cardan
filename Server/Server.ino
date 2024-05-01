212043823130eca953064581d4560a734c55e76e
SpareMeOfYourWrathOhGodOfTheCode <cedrik.michel-germain.1@ulaval.ca>
2024-05-01 19:17:14 -0400

    modifications LED

--- Server/Server.ino
+++ Server/Server.ino
@@ -15,6 +15,22 @@
 
 #define ULONG_MAX (LONG_MAX * 2UL + 1UL)
 
+/*Pinout for status leds*/
+constexpr unsigned COMMr = 7;  //Change pinout accordint to ESP
+constexpr unsigned COMMg = 4;
+constexpr unsigned COMMb = 6;
+
+constexpr unsigned SDr = 7;  //Change pinout accordint to ESP
+constexpr unsigned SDg = 4;
+constexpr unsigned SDb = 6;
+
+constexpr unsigned GPSr = 7;  //Change pinout accordint to ESP
+constexpr unsigned GPSg = 4;
+constexpr unsigned GPSb = 6;
+
+constexpr unsigned Toggle = 5;
+
+
 bool CompareMacAddress(const uint8_t * mac0, const uint8_t * mac1) {
   return (mac0[0] == mac1[0] &&
         mac0[1] == mac1[1] &&
@@ -23,6 +39,29 @@ bool CompareMacAddress(const uint8_t * mac0, const uint8_t * mac1) {
         mac0[4] == mac1[4] &&
         mac0[5] == mac1[5]);
 }
+
+class RGB_LED{
+  public:
+  RGB_LED(int redPin, int greenPin, int bluePin) : redPin(redPin) , greenPin(greenPin), bluePin(bluePin){
+  }
+
+  void selectColour(int redValue, int greenValue, int blueValue){
+    analogWrite(redPin, redValue);
+    analogWrite(greenPin, greenValue);
+    analogWrite(bluePin, blueValue);
+  }
+
+  private:
+  int redPin;
+  int greenPin;
+  int bluePin;
+}
+
+
+/*Define LEDs*/
+comLED = RGB_LED(COMMr, COMMg, COMMb);
+sdLED = RGB_LED(SDr, SDg, SDb);
+GPSLED = RGB_LED(GPSr, GPSg, GPSb);
 
 class Capteur {
 private:
@@ -56,14 +95,67 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
       Serial.print(compression.d[0]);
       Serial.print("\tValue 2: ");
       Serial.println(compression.d[1]);
-      break;
-    }
+
+      bool isRecording = digitalRead(Toggle);
+
+      sdLED.selectColour(255, 255, 1)  //jaune, enregistrement en cours
+
+      if(isRecording){
+        if(!isOpen){
+          // Open the file at the beginning of the program
+        
+          String filename = "data";
+          do {
+            filename = "data";
+            filename += String(fileCount);
+            filename += ".csv";
+
+            fileCount++;
+          }while(SD.exists(filename));
+
+          myFile = SD.open(filename, FILE_WRITE);
+          if (myFile) {
+            D_println("File opened successfully");
+            isOpen = true;
+
+            delay(20);
+          } else {
+            D_println("Error opening file");
+            digitalWrite(RedLed1, HIGH);
+          }
+      }
+          MyFile.println(compression.d[0])
+          MyFile.println(compression.d[1])
+
+          }else{
+            myFile.close();
+            isOpen = false;
+          }
+
+          delay(20);
+              break;
+            }
   }
 }
  
 void setup() {
   // Set up Serial Monitor
   Serial.begin(115200);
+
+  //carte SD
+  pinMode(RedLed1, OUTPUT);
+  pinMode(RedLed2, OUTPUT);
+
+  pinMode(YellowLed, OUTPUT);
+  pinMode(Toggle, INPUT);
+
+  /* SD */
+  while (!SD.begin(SD_ChipSelect)) {
+    D_println("Card failed, or not present");
+    digitalWrite(RedLed1, HIGH);
+    delay(100);
+  }
+  D_println("Card initialized.");
   
   // Set ESP32 as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
